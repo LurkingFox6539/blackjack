51949a7aa6ba8122e67c3e4ade741a446740e7ba
LurkingFox6539 <74066423+LurkingFox6539@users.noreply.github.com>
2021-11-08 12:02:44 -0700

    Update deck.h

--- deck.h
+++ deck.h
@@ -1,6 +1,9 @@
 /*	modified November 8 2021, Mark Wheeler III
 *		changed card strings to their numbers and letter instead of words
 *		changed strings for suits to symbols for the suits
+*		removed outputs and added the playerHand vector
+*		also split the deck initializing and shuffling and added 
+*		facedown bool to card struct
 */
 
 #ifndef _DECK_
@@ -10,24 +13,32 @@
 //Make the deck the cards, etc.
 
 #include <iostream>
-#include <string>
 #include <stdio.h>
+#include <vector>
+#include <string>
 #include <ctime>
 using namespace std;
 
 
 struct DefineCard {
-	char suit;
-	string face;
-	int pointsValue; //Face => 10 -- Ace's => 1 or 11
-	int cardStatus; // 0 = In play || 1 = In Deck || 2 = In discard
+	char suit = '\0';
+	string face = "\0";
+	int cardStatus = 0;
+	int pointsValue = 0;
+	bool facedown = false;
 }
 
 Deck[53]; // Deck [52] used for shuffling.
+vector<DefineCard> playerHand;
 
+//	First input is the hand (i.e. playerHand) and second is the card to add (i.e. Deck[0])
+//	-Not complete but functional
+void addCard(vector<DefineCard>& hand, DefineCard& card) {
+	hand.push_back(card);
+}
 
-void cardDeck() {
-	srand((unsigned)time(0));
+//	Initializes all the cards in the deck to their base values
+void initializeDeck() {
 	int New_Suit = 0;
 	string faces[13] = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
 	string Status[3] = { "In Play", "In Deck", "Discard Pile" };
@@ -43,13 +54,10 @@ void cardDeck() {
 		if ((x + 1) % 13 == 0)
 			New_Suit++;
 	}
-	cout << "Straight deck of cards.." << endl << endl;
-	for (int x = 0; x < 52; x++)
-	{
-		//cout << Deck[x].face << " of " << Deck[x].suit << "   " << Deck[x + 1].face << " of " << Deck[x + 1].suit << "  " << Deck[x + 2].face << " of " << Deck[x + 2].suit << "   " << Deck[x + 3].face << " of " << Deck[x + 3].suit << endl;
-		cout << Deck[x].face << " of " << Deck[x].suit << " : Value of " << Deck[x].pointsValue << " : " << Status[Deck[x].cardStatus] << endl;
-	}
-
+}
+	
+void shuffleDeck() {
+	srand((unsigned)time(0));
 	for (int x = 0; x < 600; x++)
 	{
 		int a = rand() % 52;
@@ -70,17 +78,6 @@ void cardDeck() {
 		Deck[b].pointsValue = Deck[52].pointsValue;
 		Deck[b].cardStatus = Deck[52].cardStatus;
 	}
-
-	cout << endl << "Shuffled deck of cards.." << endl << endl;
-
-	for (int x = 0; x < 52; x++)
-	{
-		//cout << Deck[x].face << " of " << Deck[x].suit << "   " << Deck[x + 1].face << " of " << Deck[x + 1].suit << "   " << Deck[x + 2].face << " of " << Deck[x + 2].suit << "   " << Deck[x + 3].face << " of " << Deck[x + 3].suit << endl;
-		cout << Deck[x].face << " of " << Deck[x].suit << " : Value of " << Deck[x].pointsValue << " : " << Status[Deck[x].cardStatus] << endl;
-	}
-
-	cout << endl << endl;
-	cin >> New_Suit;
 }
 
 #endif
