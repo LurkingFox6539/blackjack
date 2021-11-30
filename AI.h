ab30ff2b3b5363e8f65dae53ace8e88037d42f47
LurkingFox6539 <74066423+LurkingFox6539@users.noreply.github.com>
2021-11-30 09:21:52 -0700

    Update AI.h

--- AI.h
+++ AI.h
@@ -1,65 +1,79 @@
-/* AI for House
- * Function: add cards to house hand in attempt to beat the player unless it busts, or the players busts, or if the player has a 21. 
- */
-
-//NEITHER OLD NOR NEW AI
-
 #pragma once
-
 #ifndef _AI_
 #define _AI_
 
-#include "deck.h";
+#include "deck.h"
 #include "functions.h"
+using namespace std;
+
+void setValue();
 
 int makeDecision() {
-	int playerValue = 0;
-	int houseValue = 0;
-	for (int i = 0; i < size(players[1].hand); i++) {
-		playerValue += players[1].hand.at(i).pointsValue;
+	setValue();
+	while (players[0].handValue < 17) {
+		setValue();
+		addCard(players[0].hand);
+		system("cls");
+		printHand(players[0].hand);
+		printHand(players[1].hand);
+		setValue();
 	}
-	for (int i = 0; i < size(players[0].hand); i++) {
-		houseValue += players[0].hand.at(i).pointsValue;
-	}
-	while (houseValue < playerValue) {
-		for (int i = 0; i < size(players[0].hand); i++) {
-			houseValue += players[0].hand.at(i).pointsValue;
-		}
-		if (playerValue <= 21) {
-			addCard(players[0].hand);
-			system("cls");
-			sleep(0.5);
-			printHand(players[0].hand);
-			printHand(players[1].hand);
-		}
-	}
-	if (playerValue > 21) {
+	system("cls");
+	printHand(players[0].hand);
+	printHand(players[1].hand);
+
+	if (players[1].handValue > 21) {
 		cout << "Player's Hand exceeds 21, player goes bust.\n";
 		return 0;
 	}
-	else if (houseValue < playerValue) {
+	else if (players[0].handValue < players[1].handValue) {
 		cout << "Player has beat House. Congrats Player.\n";
 		return 1;
 	}
-	else if (houseValue == playerValue) {
+	else if (players[0].handValue == players[1].handValue) {
 		cout << "Player and House are equal. Bets are returned.\n";
 		return 2;
 	}
-	else if (houseValue > playerValue && houseValue < 21) {
-		cout << "House has won the game, with a value of " << houseValue << "!\n";
+	else if (players[0].handValue > players[1].handValue && players[0].handValue < 21) {
+		cout << "House has won the game, with a value of " << players[0].handValue << "!\n";
 		return 3;
 	}
-	else if (houseValue > playerValue && houseValue > 21) {
+	else if (players[0].handValue > players[1].handValue && players[0].handValue > 21) {
 		cout << "House hand exceeds 21, House goes bust. Congrats player.\n";
 		return 4;
 	}
-	else if (houseValue > playerValue && houseValue == 21) {
+	else if (players[0].handValue > players[1].handValue && players[0].handValue == 21) {
 		cout << "House hit a Black Jack. Player looses.\n";
 		return 5;
 	}
-	else if (playerValue > houseValue && playerValue == 21) {
+	else if (players[1].handValue > players[0].handValue && players[1].handValue == 21) {
 		cout << "Player hit a Black Jack. Congrats player.\n";
 		return 6;
+	}
+}
+
+void setValue(){
+	players[0].handValue = 0;
+	players[1].handValue = 0;
+	players[2].handValue = 0;
+
+	for (int i = 0; i < size(players[1].hand); i++) {
+		if (players[1].hand.at(i).face == "A") {
+			int aVal = ace.value(players[1]);
+			players[1].handValue += aVal;
+		}
+		else {
+			players[1].handValue += players[1].hand.at(i).pointsValue;
+		}
+	}
+	for (int i = 0; i < size(players[0].hand); i++) {
+		if (players[0].hand.at(i).face == "A") {
+			int aVal = ace.value(players[0]);
+			players[0].handValue += aVal;
+		}
+		else {
+			players[0].handValue += players[0].hand.at(i).pointsValue;
+		}
 	}
 }
 
