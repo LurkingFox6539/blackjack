4d119fbe391343a9cda8465ec83e430340cfe84f
F1awsy <93783135+F1awsy@users.noreply.github.com>
2021-12-07 11:33:26 -0700

    Update AI.h

--- AI.h
+++ AI.h
@@ -26,35 +26,35 @@ int makeDecision() {
 	printHand(players[0].hand);
 	printHand(players[1].hand);
 
-if (players[1].handValue > 21) {
-		cout << "Player's Hand exceeds 21, player goes bust.\n";
+	if (players[1].handValue > 21) {
+		cout << "\n Player's Hand exceeds 21, player goes bust.\n";
 		return 0;
 	}
 	else if (players[0].handValue > players[1].handValue && players[0].handValue < 21) {
-		cout << "House has won the game, with a value of " << players[0].handValue << "!\n";
+		cout << "\n House has won the game, with a value of " << players[0].handValue << "!\n";
 		return 3;
 	}
 	else if (players[0].handValue > players[1].handValue && players[0].handValue > 21) {
-		cout << "House hand exceeds 21, House goes bust. Congrats player.\n";
+		cout << "\n House hand exceeds 21, House goes bust. Congrats player.\n";
 		isWin = 1;
 		return 4;
 	}
 	else if (players[0].handValue > players[1].handValue && players[0].handValue == 21) {
-		cout << "House hit a Black Jack. Player has lost.\n";
+		cout << "\n House hit a Black Jack. Player has lost.\n";
 		return 5;
 	}
 	else if (players[1].handValue > players[0].handValue && players[1].handValue == 21) {
-		cout << "Player hit a Black Jack. Congrats player.\n";
+		cout << "\n Player hit a Black Jack. Congrats player.\n";
 		isWin = 1;
 		return 6;
 	}
 	else if (players[0].handValue < players[1].handValue) {
-		cout << "Player has beat House. Congrats Player.\n";
+		cout << "\n Player has beat House. Congrats Player.\n";
 		isWin = 1;
 		return 1;
 	}
 	else if (players[0].handValue == players[1].handValue) {
-		cout << "Player and House are equal. Bets are returned.\n";
+		cout << "\n Player and House are equal. Bets are returned.\n";
 		return 2;
 	}
 }
